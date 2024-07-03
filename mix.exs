3155832da3a126fa764b1204564f441ec2e1dee8
Blaine Sheldon <blaine.sheldon@gmail.com>
2024-07-03 15:15:18 -0700

    Bump req version for compatability

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule Stytch.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:req, "~> 0.3.1"},
+      {:req, "~> 0.5"},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
